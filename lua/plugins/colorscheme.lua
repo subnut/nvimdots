354917d5bcdcc3d5f1585e430c418fce720d7e83
Subhaditya Nath <sn03.general@gmail.com>
2024-06-16 16:46:39 +0530

    Improved colorscheme configuration

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,54 @@
-return require'plugins.colorschemes.iceberg'
+local idx = 1 --[[
+
+  1. iceberg
+  2. gruvbox.nvim
+  3. gruvbox-material
+
+]]
+
+local tbl = {}
+local arr = {}
+
+function add(n)
+  table.insert(arr, n)
+  return function(x)
+    table.insert(tbl, x)
+  end
+end
+
+
+add'iceberg'{
+  "cocopon/iceberg.vim",
+}
+add'gruvbox'{
+  "ellisonleao/gruvbox.nvim",
+  config = true,
+}
+add'gruvbox-material'{
+  "sainnhe/gruvbox-material",
+  opts = {
+    enable_bold = true,
+    enable_italic = true,
+  },
+  config = function(_, opts)
+    for k, v in pairs(opts) do
+      vim.g["gruvbox_material_"..k] = v
+    end
+  end,
+}
+
+
+local main = tbl[idx] -- The main (startup) colorscheme
+main.priority = 1000
+main.lazy = false
+
+local init = main.init
+main.init = main.init
+  and function() init()
+    vim.cmd.colorscheme(arr[idx])
+  end
+  or function()
+    vim.cmd.colorscheme(arr[idx])
+  end
+
+return tbl
--- lua/plugins/colorschemes/iceberg.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "cocopon/iceberg.vim",
-  event = "UIEnter",
-  init = function()
-    vim.cmd("colorscheme iceberg | hi Comment gui=italic")
-  end
-}
