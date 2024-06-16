9bab84e5c23d39a2122da3372eae8325b66c5e92
Subhaditya Nath <sn03.general@gmail.com>
2024-06-16 20:21:13 +0530

    [plugins] Added nvim-lspconfig

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,31 @@
+local T = {}
+if not mycfg.lsp then
+  return T
+end
+
+local servers = {
+  --[[ C ]]     "clangd",
+  --[[ Py ]]    "pylsp", "basedpyright",
+  --[[ Zig ]]   "zls",
+
+  --[[ D ]]     {"serve-d", "serve_d"},
+  --[[ Lua ]]   {"lua-language-server", "lua_ls"},
+  --[[ Rust ]]  {"rust-analyzer", "rust_analyzer"},
+  --[[ Typst ]] {"typst-lsp", "typst_lsp"},
+}
+
+local config = {}
+
+T = {"neovim/nvim-lspconfig"}
+T.config = function(_, __)
+  for _,server in ipairs(servers) do
+    local exe = type(server) == "string" and server or server[1]
+    local lsp = type(server) == "string" and server or server[2]
+    local cfg = config[lsp] or {}
+    if vim.fn.executable(exe) then
+      require'lspconfig'[lsp].setup(cfg)
+    end
+  end
+end
+
+return T
