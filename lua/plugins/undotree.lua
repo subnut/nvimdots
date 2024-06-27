c4ba7dd2c53a9962b51ae591302fc8a31f190ed6
Subhaditya Nath <sn03.general@gmail.com>
2024-06-27 15:38:50 +0530

    undotree: refactor configuration

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,22 @@
-return {
+local T = {
   "mbbill/undotree",
   cmd = "UndotreeToggle",
-  keys = {
-    { "<leader>u", "<cmd>UndotreeToggle<CR>", desc = "Toggle UndoTree" },
-  }
+  keys = { insert = table.insert },
 }
+
+T.opts = {
+  WindowLayout = 2,
+}
+
+T.keys:insert({
+  "<leader>u", "<cmd>UndotreeToggle<CR>",
+  desc = "Toggle UndoTree"
+})
+
+T.config = function(_, opts)
+  for k, v in pairs(opts) do
+    vim.g["undotree_"..k] = v
+  end
+end
+
+return T
