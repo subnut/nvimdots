7a7e44bc1591efc67be340bb7f2370d336ab948e
Subhaditya Nath <sn03.general@gmail.com>
2024-06-28 01:54:22 +0530

    added project.nvim, added telescope keymapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,25 @@
 local T = {
   "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
   version = "*",
+}
+
+T.cmd = "Telescope"
+T.keys = {{
+  "<leader>t",
+  "<cmd>Telescope<CR>",
+  { desc = "Open Telescope" },
+}}
+T.config = function(_, opts)
+  require'telescope'.setup(opts)
+  require'telescope'.load_extension'projects'
+end
+T.dependencies = {
+  {"nvim-lua/plenary.nvim"},
+  {"ahmedkhalf/project.nvim",
+    opts = { manual_mode = true },
+    main = "project_nvim",
+    cmd = "ProjectRoot",
+  },
 }
 
 return T
